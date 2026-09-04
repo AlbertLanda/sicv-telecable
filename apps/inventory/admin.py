f8da094a5aae196ad9a5e8a3f773be0f1edb959f
Andrews LR <108280293+AlbertLanda@users.noreply.github.com>
2026-09-04 14:50:59 -0500

    feat: administrar catalogo y movimientos de materiales

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.inventory.models import Material, WorkOrderMaterialMovement
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "unit_of_measure", "is_active")
+    list_filter = ("unit_of_measure", "is_active")
+    search_fields = ("code", "name")
+    ordering = ("name",)
+
+
+@admin.register(WorkOrderMaterialMovement)
+class WorkOrderMaterialMovementAdmin(admin.ModelAdmin):
+    list_display = (
+        "work_order",
+        "movement_type",
+        "material",
+        "quantity",
+        "recorded_by",
+        "updated_at",
+    )
+    list_filter = ("movement_type", "material")
+    search_fields = ("work_order__order_number", "material__code", "material__name")
+    readonly_fields = (
+        "work_order",
+        "movement_type",
+        "material",
+        "quantity",
+        "remarks",
+        "recorded_by",
+        "created_at",
+        "updated_at",
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
