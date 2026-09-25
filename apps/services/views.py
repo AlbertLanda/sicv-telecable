82e01b2375da90e27be13a60ba2878ec31680240
Andrews LR <108280293+AlbertLanda@users.noreply.github.com>
2026-09-25 11:45:47 -0500

    chore(services): retirar import de correlativo anterior

--- apps/services/views.py
+++ apps/services/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError, transaction
-from django.db.models import Count, Max, Q
+from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
